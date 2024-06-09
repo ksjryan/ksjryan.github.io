e798ed7349017fd84ac541e21679b0ba77faecb0
Seongjun-Kang <81222069+ksjryan@users.noreply.github.com>
2024-06-09 17:53:54 +0900

    Update smooth-scroll.js

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
-
-      const target = document.querySelector(this.getAttribute('href'));
-      const topOffset = target.getBoundingClientRect().top + window.pageYOffset;
-      
-      window.scrollTo({
-        top: topOffset,
-        behavior: 'smooth'
-      });
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth'
+        });
+      }
     });
   });
 });
